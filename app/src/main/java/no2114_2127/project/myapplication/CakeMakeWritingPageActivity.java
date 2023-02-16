a17e388408211793eaed1c408bff59e078c25ed9
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-02-17 00:05:47 +0900

    케이크 롤링페이퍼 작성 페이지 상장 작성 페이지 작업 함

---
+++ app/src/main/java/no2114_2127/project/myapplication/CakeMakeWritingPageActivity.java
@@ -0,0 +1,14 @@
+package no2114_2127.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CakeMakeWritingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cake_make_writing_page);
+    }
+}
