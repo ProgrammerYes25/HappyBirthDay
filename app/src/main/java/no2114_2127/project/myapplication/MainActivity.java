61793350378aaed0fe20a3d70b3c6d595b7f7691
jihye1012 <s2127@e-mirim.hs.kr>
2023-04-23 21:51:06 +0900

    main 페이지 레이아웃 틀 잡기

--- app/src/main/java/no2114_2127/project/myapplication/MainActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/MainActivity.java
@@ -1,14 +1,18 @@
 package no2114_2127.project.myapplication;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView toggleDeco=findViewById(R.id.toggle_tv_deco);
         //Y-초기커밋~~~!!
         //C-테스트커밋!!
+        toggleDeco.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG) ;
     }
 }
