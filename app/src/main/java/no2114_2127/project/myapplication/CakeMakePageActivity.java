3cba4122546d84422eac68886dfaa2df02f1f1a0
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-05-10 01:42:51 +0900

    #14 카드 보기 페이지 레이아웃 구성 확인을 위한 코드 수정

--- app/src/main/java/no2114_2127/project/myapplication/CakeMakePageActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/CakeMakePageActivity.java
@@ -9,6 +9,6 @@ public class CakeMakePageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cake_make_writing_page);
+        setContentView(R.layout.activity_cerd_show_page);
     }
 }
