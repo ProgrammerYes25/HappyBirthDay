b0b0aef2f20da20e2cb8cd3953170cb9c41b0dc3
yes25 <programmer.yes25@gmail.com>
2023-05-11 18:34:50 +0900

    #14 레이아웃 정리 중

---
+++ app/src/main/java/no2114_2127/project/myapplication/CakeMakePageFragment.java
@@ -0,0 +1,11 @@
+package no2114_2127.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class CakeMakePageFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/no2114_2127/project/myapplication/CakeMakeWritingPageFragment.java
@@ -0,0 +1,8 @@
+package no2114_2127.project.myapplication;
+
+
+
+import androidx.fragment.app.Fragment;
+
+public class CakeMakeWritingPageFragment extends Fragment {
+}
