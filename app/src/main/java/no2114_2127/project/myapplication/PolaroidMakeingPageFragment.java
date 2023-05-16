5f611274ed68d5d8690daee21ff075943bcdd04a
yes25 <programmer.yes25@gmail.com>
2023-05-16 12:11:10 +0900

    #15 fragment_polaroid_makeing_page.xml 레이아웃 구성 완료

---
+++ app/src/main/java/no2114_2127/project/myapplication/PolaroidMakeingPageFragment.java
@@ -0,0 +1,8 @@
+package no2114_2127.project.myapplication;
+
+import android.app.Activity;
+
+import androidx.fragment.app.Fragment;
+
+public class PolaroidMakeingPageFragment extends Fragment {
+}
