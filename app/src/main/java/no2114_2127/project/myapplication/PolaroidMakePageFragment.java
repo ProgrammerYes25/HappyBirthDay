5b9a1ef5af67af487ac821a7f63859967f6d8eb4
yes25 <programmer.yes25@gmail.com>
2023-05-16 09:40:19 +0900

    #15 fragment_polaroid_make_page.xml 레이아웃 구성 완료

--- app/src/main/java/no2114_2127/project/myapplication/PolaroidMakePageFragment.java
+++ app/src/main/java/no2114_2127/project/myapplication/PolaroidMakePageFragment.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PolaroidMakePageActivity extends AppCompatActivity {
+public class PolaroidMakePageFragment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_polaroid_make_page);
+        setContentView(R.layout.fragment_polaroid_make_page);
     }
 }
