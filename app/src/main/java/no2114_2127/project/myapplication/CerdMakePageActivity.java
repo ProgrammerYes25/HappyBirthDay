1daf6a26ae335feaf749576f8f8be4ea848d33db
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-05-21 01:17:39 +0900

    #16 cerdMarePage 상단바 작업 완료

--- app/src/main/java/no2114_2127/project/myapplication/CerdMakePageActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/CerdMakePageActivity.java
@@ -1,14 +1,21 @@
 package no2114_2127.project.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class CerdMakePageActivity extends AppCompatActivity {
-
+    Fragment CakeMakePageFragment, CakeMakeingPageFragment, CakeMakeWritingPageFragment,
+            PolaroidMakePageFragment, PolaroidMakeingPageFragment,
+            VideoUploadPragment, VideoUploadingPragment,
+            AwardMakePageFragment, AwardMakeChooseFragment, AwardMakeChooseFormFragment, AwardMakeWritingPageFragment,
+            FromFinishPageFragment;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cerd_make_page);
+
     }
 }
