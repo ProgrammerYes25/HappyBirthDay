545a0e91ab0f727fc96de64a79becc1de5df3aa3
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-05-21 12:49:04 +0900

    #16 cerdMarePage 상단바 뒤로가기 버튼 작업 완료

--- app/src/main/java/no2114_2127/project/myapplication/CerdMakePageActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/CerdMakePageActivity.java
@@ -4,18 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CerdMakePageActivity extends AppCompatActivity {
+    String CerdMakeTitle = "의 생일카드";
+    //Layout
+    FrameLayout cerdMakeFrameLayout;
+    //TextView
+    TextView cerdMakeSubtitleTextView;
+    //ImageView
+    ImageView backButtonImageView;
     Fragment CakeMakePageFragment, CakeMakeingPageFragment, CakeMakeWritingPageFragment,
             PolaroidMakePageFragment, PolaroidMakeingPageFragment,
             VideoUploadPragment, VideoUploadingPragment,
             AwardMakePageFragment, AwardMakeChooseFragment, AwardMakeChooseFormFragment, AwardMakeWritingPageFragment,
             FromFinishPageFragment;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cerd_make_page);
+        // Layout find View by id
+        cerdMakeFrameLayout = findViewById(R.id.cerd_make_frame_layout);
+        // textView find View by id
+        cerdMakeSubtitleTextView = findViewById(R.id.cerd_make_subtitle_text_view);
+        // ImageView find View by id
+        backButtonImageView = findViewById(R.id.back_button_image_view);
+        // cerdMakeSubtitleTextView.setVisibility(View.VISIBLE);
 
+        // setOnClick
+        // backButtonImageView setOnClick
+        backButtonImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
