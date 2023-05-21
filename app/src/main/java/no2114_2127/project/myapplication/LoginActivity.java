2af7c9e2e2509b7d763c545bc0a070b8235e63fb
jihye1012 <s2127@e-mirim.hs.kr>
2023-05-22 00:13:45 +0900

    #18 로그인 페이지 레이아웃 수정 완료 (그라데이션 색 안나옴)

--- app/src/main/java/no2114_2127/project/myapplication/LoginActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/LoginActivity.java
@@ -2,6 +2,7 @@ package no2114_2127.project.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
