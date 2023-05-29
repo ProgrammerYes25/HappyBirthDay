f202538d59ab022f91f40d52240ab8357da495a8
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-05-30 00:59:46 +0900

    #22 PolaroidMakePage 레이아웃 수정

--- app/src/main/java/no2114_2127/project/myapplication/PolaroidMakePageFragment.java
+++ app/src/main/java/no2114_2127/project/myapplication/PolaroidMakePageFragment.java
@@ -1,14 +1,26 @@
 package no2114_2127.project.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class PolaroidMakePageFragment extends AppCompatActivity {
+public class PolaroidMakePageFragment extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_polaroid_make_page);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_polaroid_make_page, container, false);
+        return view;
+    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+    public static PolaroidMakePageFragment PolaroidMakePageInstance() {
+        return new PolaroidMakePageFragment();
     }
 }
