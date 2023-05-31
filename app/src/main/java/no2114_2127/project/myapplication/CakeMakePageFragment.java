aecf81224f33cd88c2903a58fd03b08bfcf33455
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-06-01 06:40:19 +0900

    #27 cerdMakePage 버튼 작업중

--- app/src/main/java/no2114_2127/project/myapplication/CakeMakePageFragment.java
+++ app/src/main/java/no2114_2127/project/myapplication/CakeMakePageFragment.java
@@ -9,12 +9,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CakeMakePageFragment extends Fragment {
+    // button
+    TextView decorationButtonTextView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cake_make_page, container, false);
+
+        //button find view by id
+        decorationButtonTextView = view.findViewById(R.id.decoration_button_text_view);
+        decorationButtonTextView.setOnClickListener(onClickListener);
         return view;
     }
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -23,4 +33,14 @@ public class CakeMakePageFragment extends Fragment {
     public static CakeMakePageFragment cakeMakePageInstance() {
         return new CakeMakePageFragment();
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.fragment_cake_make_page:
+
+            }
+        }
+    };
 }
