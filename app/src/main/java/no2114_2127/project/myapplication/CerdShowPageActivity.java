00fda5c96091770ac213057cfa1f160487187254
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-06-01 00:05:21 +0900

    #26 cerdShowPage 레이아웃 생성 완료

---
+++ app/src/main/java/no2114_2127/project/myapplication/CerdShowPageActivity.java
@@ -0,0 +1,55 @@
+package no2114_2127.project.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class CerdShowPageActivity extends AppCompatActivity {
+    String CerdMakeTitle = "의 생일카드";
+
+    int cerdMakeFrameLayoutId;
+    //Layout
+    FrameLayout cerdMakeFrameLayout;
+    //TextView
+    TextView cerdMakeSubtitleTextView;
+    //ImageView
+    ImageView backButtonImageView;
+    Fragment CakeMakePageFragment, CakeMakeingPageFragment, CakeMakeWritingPageFragment,
+            PolaroidMakePageFragment, PolaroidMakeingPageFragment,
+            VideoUploadFragment, VideoUploadingPragment,
+            AwardMakePageFragment, AwardMakeChooseFragment, AwardMakeChooseFormFragment, AwardMakeWritingPageFragment,
+            FromFinishPageFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cerd_show_page);
+        cerdMakeFrameLayoutId = R.id.cerd_make_frame_layout;
+        // Layout find View by id
+        cerdMakeFrameLayout = findViewById(R.id.cerd_make_frame_layout);
+        // textView find View by id
+        // ImageView find View by id
+        backButtonImageView = findViewById(R.id.back_button_image_view);
+
+        // Fragent
+        CakeMakePageFragment = new CakeMakePageFragment();
+        PolaroidMakePageFragment = new PolaroidMakePageFragment();
+        AwardMakePageFragment = new AwardMakePageFragment();
+        VideoUploadFragment = new VideoUploadFragment();
+        // setOnClick
+        // backButtonImageView setOnClick
+        backButtonImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().add(R.id.cerd_make_frame_layout, CakeMakePageFragment).commitAllowingStateLoss();
+    }
+}
