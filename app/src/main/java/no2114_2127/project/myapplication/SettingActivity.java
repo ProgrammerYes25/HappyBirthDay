c7648a6f202a76d11dc42ea829e965374be82c53
jihye1012 <s2127@e-mirim.hs.kr>
2023-06-07 04:36:04 +0900

    #37 custom switch대신 일반 switch 추가

--- app/src/main/java/no2114_2127/project/myapplication/SettingActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/SettingActivity.java
@@ -3,12 +3,28 @@ package no2114_2127.project.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingActivity extends AppCompatActivity {
-
+    int nChecked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Switch switchButton = findViewById(R.id.setting_switch);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    // switchButton이 체크된 경우
+                    nChecked = 1;
+                } else {
+                    // switchButton이 체크되지 않은 경우
+                    nChecked = 0;
+                }
+            }
+        });
     }
 }
