d5aee8e32a2273f847f58d4d63b641f96f4fe1d8
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-06-06 21:51:24 +0900

    #36 로그인 여부 확인 하는 작업 완료

--- app/src/main/java/no2114_2127/project/myapplication/MainActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/MainActivity.java
@@ -1,11 +1,17 @@
 package no2114_2127.project.myapplication;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth; //FirevaseAuth 객체 선언
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
         //Y-초기커밋~~~!!
         //C-테스트커밋!!
         toggleDeco.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG) ;
+
+        mAuth = FirebaseAuth.getInstance(); //FirevaseAuth 객체 정의
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){    //로그인이 되어있지 않은면 SignUpActivity를 실행 시킴
+            startActivityM(LoginActivity.class);
+        }
+    }
+    private void startActivityM(Class activityClass){ //activity전환 메소드
+        Intent intent = new Intent(this, activityClass);
+        startActivity(intent);
     }
 }
--- app/src/main/java/no2114_2127/project/myapplication/SignUpActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/SignUpActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth; //FirevaseAuth 객체 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
