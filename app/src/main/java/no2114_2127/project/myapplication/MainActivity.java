07b5901b17233e4558cdfe9dce6c5b3a91094b34
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-06-08 22:25:19 +0900

    #28 위에 사각형 마진 해결

--- app/src/main/java/no2114_2127/project/myapplication/MainActivity.java
+++ app/src/main/java/no2114_2127/project/myapplication/MainActivity.java
@@ -17,15 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView toggleDeco=findViewById(R.id.toggle_tv_deco);
-        //Y-초기커밋~~~!!
+        //Y-초기커밋~~~!!ㅣㅐㅣㅡ
         //C-테스트커밋!!
 
 
         mAuth = FirebaseAuth.getInstance(); //FirevaseAuth 객체 정의
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null){    //로그인이 되어있지 않은면 SignUpActivity를 실행 시킴
-            startActivityM(LoginActivity.class);
-        }
+//        if(currentUser == null){    //로그인이 되어있지 않은면 SignUpActivity를 실행 시킴
+//            startActivityM(LoginActivity.class);
+//        }
     }
     private void startActivityM(Class activityClass){ //activity전환 메소드
         Intent intent = new Intent(this, activityClass);
