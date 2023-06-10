a937690eeb5e8f90117abc5377321576d1c1292e
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-06-10 23:52:42 +0900

    #39 polaroidFrame 바뀌는 거 성공

--- app/src/main/java/no2114_2127/project/myapplication/PolaroidMakeingPageFragment.java
+++ app/src/main/java/no2114_2127/project/myapplication/PolaroidMakeingPageFragment.java
@@ -6,19 +6,70 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PolaroidMakeingPageFragment extends Fragment {
+    //text View 선언
     TextView polaroidButtonTextView;
+
+    // image view 선언
+    ImageView polaroidImageView, polaroidFrame1, polaroidFrame2, polaroidFrame3,
+            polaroidFrame4, polaroidFrame5, polaroidFrame6,
+            polaroidFrame7, polaroidFrame8, polaroidFrame9;
+    // editText
+    EditText polaroidEditText;
+
+    // polaroid frame item 배열
+    int polaroidFrame[] = {R.drawable.polaroid_frame_1, R.drawable.polaroid_frame_2, R.drawable.polaroid_frame_3,
+            R.drawable.polaroid_frame_4, R.drawable.polaroid_frame_5,R.drawable.polaroid_frame_6,
+            R.drawable.polaroid_frame_7, R.drawable.polaroid_frame_8, R.drawable.polaroid_frame_9 };
+    int polaroidFrameItem[] = {R.drawable.polaroid_frame_item_1, R.drawable.polaroid_frame_item_2, R.drawable.polaroid_frame_item_3,
+            R.drawable.polaroid_frame_item_4, R.drawable.polaroid_frame_item_5, R.drawable.polaroid_frame_item_6,
+            R.drawable.polaroid_frame_item_7, R.drawable.polaroid_frame_item_8, R.drawable.polaroid_frame_item_9};
+    // polaroid frame on item 배열
+    int polaroidFrameOnItem[] = {R.drawable.polaroid_frame_on_item_1, R.drawable.polaroid_frame_on_item_2, R.drawable.polaroid_frame_on_item_3,
+            R.drawable.polaroid_frame_on_item_4, R.drawable.polaroid_frame_on_item_5, R.drawable.polaroid_frame_on_item_6,
+            R.drawable.polaroid_frame_on_item_7, R.drawable.polaroid_frame_on_item_8, R.drawable.polaroid_frame_on_item_9};
+    ImageView choiceImageView;
+    int choiceindex = 0;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_polaroid_makeing_page, container, false);
+        // image view find View by id
+        polaroidFrame1 = view.findViewById(R.id.polaroid_frame_1);
+        polaroidFrame2 = view.findViewById(R.id.polaroid_frame_2);
+        polaroidFrame3 = view.findViewById(R.id.polaroid_frame_3);
+        polaroidFrame4 = view.findViewById(R.id.polaroid_frame_4);
+        polaroidFrame5 = view.findViewById(R.id.polaroid_frame_5);
+        polaroidFrame6 = view.findViewById(R.id.polaroid_frame_6);
+        polaroidFrame7 = view.findViewById(R.id.polaroid_frame_7);
+        polaroidFrame8 = view.findViewById(R.id.polaroid_frame_8);
+        polaroidFrame9 = view.findViewById(R.id.polaroid_frame_9);
+        polaroidImageView = view.findViewById(R.id.polaroid_image_view);
+        // text view find View by id
         polaroidButtonTextView = view.findViewById(R.id.polaroid_button_text_view);
         polaroidButtonTextView.setOnClickListener(onClickListener);
+
+        // EditText find View by id
+        polaroidEditText = view.findViewById(R.id.polaroid_edit_text);
+
+        // image view setOnClick
+        polaroidFrame1.setOnClickListener(onClickListener);
+        polaroidFrame2.setOnClickListener(onClickListener);
+        polaroidFrame3.setOnClickListener(onClickListener);
+        polaroidFrame4.setOnClickListener(onClickListener);
+        polaroidFrame5.setOnClickListener(onClickListener);
+        polaroidFrame6.setOnClickListener(onClickListener);
+        polaroidFrame7.setOnClickListener(onClickListener);
+        polaroidFrame8.setOnClickListener(onClickListener);
+        polaroidFrame9.setOnClickListener(onClickListener);
+        choiceImageView = polaroidFrame1;
         return view;
     }
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -39,7 +90,44 @@ public class PolaroidMakeingPageFragment extends Fragment {
                     StageClass.stage =2;
                     startActivity(intent);
                     break;
+                case R.id.polaroid_frame_1:
+                    changePolaroidFrame(0, polaroidFrame1);
+                    break;
+                case R.id.polaroid_frame_2:
+                    changePolaroidFrame(1, polaroidFrame2);
+                    break;
+                case R.id.polaroid_frame_3:
+                    changePolaroidFrame(2, polaroidFrame3);
+                    break;
+                case R.id.polaroid_frame_4:
+                    changePolaroidFrame(3, polaroidFrame4);
+                    break;
+                case R.id.polaroid_frame_5:
+                    changePolaroidFrame(4, polaroidFrame5);
+                    break;
+                case R.id.polaroid_frame_6:
+                    changePolaroidFrame(5, polaroidFrame6);
+                    break;
+                case R.id.polaroid_frame_7:
+                    changePolaroidFrame(6, polaroidFrame7);
+                    break;
+                case R.id.polaroid_frame_8:
+                    changePolaroidFrame(7, polaroidFrame8);
+                    break;
+                case R.id.polaroid_frame_9:
+                    changePolaroidFrame(8, polaroidFrame9);
+                    break;
             }
         }
     };
+
+    public void changePolaroidFrame(int index, ImageView polaroidFrameItemImageView){
+        if(choiceindex!=index){
+            polaroidImageView.setImageResource(polaroidFrame[index]);
+            polaroidFrameItemImageView.setImageResource(polaroidFrameOnItem[index]);
+            choiceImageView.setImageResource(polaroidFrameItem[choiceindex]);
+            choiceImageView = polaroidFrameItemImageView;
+            choiceindex = index;
+        }
+    }
 }
