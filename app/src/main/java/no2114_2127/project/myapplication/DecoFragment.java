e10d092e402de6b6a2f449ad401034b9c1d369b0
jihye1012 <s2127@e-mirim.hs.kr>
2023-06-17 01:11:20 +0900

    #28 메인페이지 꾸며주기에서 링크 추가 EditText가 비어있을시 버튼 비활성화 진행중...

--- app/src/main/java/no2114_2127/project/myapplication/DecoFragment.java
+++ app/src/main/java/no2114_2127/project/myapplication/DecoFragment.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,9 +33,12 @@ public class DecoFragment extends Fragment {
     EditText inputLink;
     TextView cardName;
     TextView nameBirth;
+    String inputText;
+
 
     private CustomAdapter MainDecoGridAdapter;
-
+    TextView noBtn;
+    TextView yesBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_deco, container, false);
@@ -47,6 +52,9 @@ public class DecoFragment extends Fragment {
         addLink.setContentView(R.layout.dialog_input_link);             // xml 레이아웃 파일과 연결
         MainDecoGridAdapter = new CustomAdapter(requireContext(), getData());
         inputLink=view2.findViewById(R.id.put_text);
+        noBtn = addLink.findViewById(R.id.btn_cancel);
+        yesBtn = addLink.findViewById(R.id.btn_add);
+        inputText = inputLink.getText().toString();
         view.findViewById(R.id.link_add_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,10 +74,13 @@ public class DecoFragment extends Fragment {
         return data;
     }
     public void showAddLink(){
+
         addLink.show(); // 다이얼로그 띄우기
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
             Objects.requireNonNull(addLink.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         }
+
+
         /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
 
         // 위젯 연결 방식은 각자 취향대로~
@@ -78,11 +89,11 @@ public class DecoFragment extends Fragment {
         // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
 
         // 취소 버튼
-        TextView noBtn = addLink.findViewById(R.id.btn_cancel);
-        TextView yesBtn = addLink.findViewById(R.id.btn_add);
+
         noBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 // 원하는 기능 구현
                 addLink.dismiss(); // 다이얼로그 닫기
             }
@@ -91,19 +102,55 @@ public class DecoFragment extends Fragment {
         yesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 원하는 기능 구현
-                    // GridView에 추가할 아이템 생성
-//                String inputText = inputLink.getText().toString();
-//                if (!inputText.isEmpty()) {
-                    // 생성한 레이아웃 아이템을 그리드뷰에 추가
-                        MainDecoGridAdapter.addItem(new MainDecoListItem(cardName,nameBirth));
-                        MainDecoGridAdapter.notifyDataSetChanged();
-                        addLink.dismiss();
-                    //}
+                if (inputText.isEmpty()) {
+                    // 입력된 텍스트가 비어 있는 경우
+                    yesBtn.setEnabled(false); // 추가 버튼 비활성화
+                    // yesBtn.setBackground(null);
+                    yesBtn.setBackground(getResources().getDrawable(R.drawable.rectangle_resource_activation_radadd_enabled));
+                } else {
+                    // 입력된 텍스트가 있는 경우
+                    yesBtn.setEnabled(true); // 추가 버튼 활성화
+                    //yesBtn.setBackground(null);
+                    yesBtn.setBackground(getResources().getDrawable(R.drawable.rectangle_resource_activation_radadd));
+                    MainDecoGridAdapter.addItem(new MainDecoListItem(cardName, nameBirth));
+                    MainDecoGridAdapter.notifyDataSetChanged();
+                    addLink.dismiss();
+
                 }
 
+                    //inputLink.setError("입력이 필요합니다.");
 
+
+//                        MainDecoGridAdapter.addItem(new MainDecoListItem(cardName,nameBirth));
+//                        MainDecoGridAdapter.notifyDataSetChanged();
+//                        addLink.dismiss();
+                    //}
+                }
     });
+//        inputLink.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                if (editable.toString().isEmpty()) {
+//                    // 입력된 텍스트가 비어 있는 경우
+//                    yesBtn.setEnabled(false); // 추가 버튼 비활성화
+//                    // yesBtn.setBackground(null);
+//                    yesBtn.setBackground(getResources().getDrawable(R.drawable.rectangle_resource_activation_radadd_enabled));
+//                } else {
+//                    // 입력된 텍스트가 있는 경우
+//                    yesBtn.setEnabled(true); // 추가 버튼 활성화
+//                    //yesBtn.setBackground(null);
+//                    yesBtn.setBackground(getResources().getDrawable(R.drawable.rectangle_resource_activation_radadd));
+//                }
+//            }
+//        });
     }
 
 }
