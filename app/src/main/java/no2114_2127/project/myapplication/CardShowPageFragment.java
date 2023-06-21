31ed8cd86879fb82a8ada65b08b5cb807ee08e86
jihye1012 <s2127@e-mirim.hs.kr>
2023-06-21 12:17:44 +0900

    #45 fragment에 레이아웃 파일 아이디 연결

--- app/src/main/java/no2114_2127/project/myapplication/CardShowPageFragment.java
+++ app/src/main/java/no2114_2127/project/myapplication/CardShowPageFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,10 +15,13 @@ import androidx.fragment.app.Fragment;
 
 public class CardShowPageFragment extends Fragment {
     // textView button
-    TextView decorationButtonTextView;
+    TextView decorationButtonTextView,pageNumTextView,makePolaroidTitleTextView,makePolaroidSubtitleTextView,
+            makeVideoTitleTextView,makeVideoSubtitleTextView,makeAwardTitleTextView,makeAwardSubtitleTextView;
     LinearLayout leftLayout, rightLayout;
     ImageView decorative1ImageView, decorative2ImageView, decorative3ImageView,
             decorative4ImageView, decorative5ImageView, decorative6ImageView;
+
+    GridView makePolaroidGridView,videoGridView,makeAwardGridView;
 
     @Nullable
     @Override
@@ -37,6 +41,20 @@ public class CardShowPageFragment extends Fragment {
         // Layout
         leftLayout = view.findViewById(R.id.left_layout);
         rightLayout = view.findViewById(R.id.right_layout);
+
+        pageNumTextView=view.findViewById(R.id.page_num_text_view);
+        makePolaroidTitleTextView=view.findViewById(R.id.make_polaroid_title_text_view);
+        makePolaroidSubtitleTextView=view.findViewById(R.id.make_polaroid_subtitle_text_view);
+        makeVideoTitleTextView=view.findViewById(R.id.make_video_title_text_view);
+        makeVideoSubtitleTextView=view.findViewById(R.id.make_video_subtitle_text_view);
+        makeAwardTitleTextView=view.findViewById(R.id.make_award_title_text_view);
+        makeAwardSubtitleTextView=view.findViewById(R.id.make_award_subtitle_text_view);
+        decorationButtonTextView=view.findViewById(R.id.decoration_button_text_view);
+
+        makePolaroidGridView=view.findViewById(R.id.make_polaroid_grid_view);
+        videoGridView=view.findViewById(R.id.video_grid_view);
+        makeAwardGridView=view.findViewById(R.id.make_award_grid_view);
+
         leftLayout.setOnClickListener(layoutOnClickListener);
         rightLayout.setOnClickListener(layoutOnClickListener);
         return view;
